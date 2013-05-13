5bf1a898cfadd0608a735d5e94df2a4b833866c8
seankay <f.sean.kay@gmail.com>
2013-05-13 18:09:14 -0400

    Initial Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blails::Application.config.secret_token = '473e4b5aa9b773bdcf3bfac3b6e1d209d8990bae552a03f649e3754a4cf9adb63bb4480fd9603bb45a494886ec33ecb0f4b64335a654d41973831718a4ec16e2'
